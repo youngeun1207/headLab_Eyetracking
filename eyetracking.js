68103c09cadb9b216e0667f2ac15c9d60233f941
youngeun1207 <yesheen98@gmail.com>
2022-08-07 20:03:07 +0900

    web gazer로 eyetracking 구현

--- eyetracking.js
+++ eyetracking.js
@@ -1,22 +1,43 @@
-// import webgazer from 'webgazer'
-const gazePoint = document.getElementById("gaze-point");
+window.onload = async function() {
 
-webgazer.setGazeListener(function(data, elapsedTime) {
-  if (data == null) {
-      return;
-  }
-  var xprediction = data.x; //these x coordinates are relative to the viewport
-  var yprediction = data.y; //these y coordinates are relative to the viewport
-  // console.log(elapsedTime); //elapsed time is based on time since begin was called
+  //start the webgazer tracker
+  await webgazer.setRegression('ridge') /* currently must set regression and tracker */
+      //.setTracker('clmtrackr')
+      .setGazeListener(function(data, clock) {
+        //   console.log(data); /* data is an object containing an x and y key which are the x and y prediction coordinates (no bounds limiting) */
+        //   console.log(clock); /* elapsed time in milliseconds since webgazer.begin() was called */
+      })
+      .saveDataAcrossSessions(true)
+      .begin();
+      webgazer.showVideoPreview(true) /* shows all video previews */
+          .showPredictionPoints(true) /* shows a square every 100 milliseconds where current prediction is */
+          .applyKalmanFilter(true); /* Kalman Filter defaults to on. Can be toggled by user. */
 
-  gazePoint.textContent=String(xprediction + " " + yprediction);
-}).begin();
+  //Set up the webgazer video feedback.
+  var setup = function() {
+      //Set up the main canvas. The main canvas is used to calibrate the webgazer.
+      var canvas = document.getElementById("plotting_canvas");
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+      canvas.style.position = 'fixed';
+  };
+  setup();
 
-webgazer.showVideo(false);
-webgazer.showPredictionPoints(false);
+};
 
-var prediction = webgazer.getCurrentPrediction();
-if (prediction) {
-    var x = prediction.x;
-    var y = prediction.y;
+// Set to true if you want to save the data even if you reload the page.
+window.saveDataAcrossSessions = true;
+
+window.onbeforeunload = function() {
+  webgazer.end();
+}
+
+/**
+* Restart the calibration process by clearing the local storage and reseting the calibration point
+*/
+function Restart(){
+  $('#helpModal').modal('hide');
+  webgazer.clearData();
+  ClearCalibration();
+  PopUpInstruction();
 }
