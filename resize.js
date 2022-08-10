2e56b29b5afeb2533ff3d5b8e0881d58030c5a1f
youngeun1207 <yesheen98@gmail.com>
2022-08-10 10:18:25 +0900

    필요없는 파일 삭제

--- resize.js
+++
@@ -1,8 +0,0 @@
-function resize() {
-    var canvas = document.getElementById('plotting_canvas');
-    var context = canvas.getContext('2d');
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-};
-window.addEventListener('resize', resize, false);
